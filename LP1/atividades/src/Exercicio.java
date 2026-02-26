b7fd98c140a07c7425ee9f115682f08669957142
fernandocosta45 <fernando.costa45@fatec.sp.gov.br>
2026-02-25 21:27:21 -0300

    Add files via upload

---
+++ LP1/atividades/src/Exercicio.java
@@ -0,0 +1,11 @@
+public class Exercicio {
+
+   /* This is my first java program.
+    * This will print 'Hello, World!' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Let is get to work,\n everyone!!"); // prints Let is get to work, everyone!!
+     
+   }
+}
---
+++ LP1/atividades/src/Exercicio2.java
@@ -0,0 +1,11 @@
+public class Exercicio2 {
+
+   /* This is my first java program.
+    * This will print 'Hello, World!' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Let is get to work, everyone!!"); // prints Let is get to work, everyone!!
+      System.out.println("It is time to program in Java!"); // prints It is time to program in Java!!
+   }
+}
