ddcc42cbb9e46ade43f6702f473750053c69576e
fernandocosta45 <fernando.costa45@fatec.sp.gov.br>
2026-03-10 22:52:56 -0300

    Add files via upload

---
+++ Atividade_1/src/Atividade_1.java
@@ -0,0 +1,11 @@
+
+public class Atividade_1 {
+
+    /* This is my first java program.
+     * This will print 'Hi, folks!' as the output
+     */
+
+    public static void main(String []args) {
+        System.out.println("Hi, folks!"); // prints Hi, folks!
+    }
+}
