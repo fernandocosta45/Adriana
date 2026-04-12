0ae359d4f1f1b3d11a0e0e8081142bc7cd26e9f4
fernandocosta45 <fernando.costa45@fatec.sp.gov.br>
2026-04-12 13:24:24 -0300

    Add files via upload

---
+++ ExerciciosComMaven/src/FeiraLivre.java
@@ -0,0 +1,25 @@
+package br.com.exemplo;
+
+public class FeiraLivre {
+    private String local;
+    private String listaProdutos;
+    private String horarioFuncionamento;
+
+    public FeiraLivre(String local, String listaProdutos, String horarioFuncionamento) {
+        this.local = local;
+        this.listaProdutos = listaProdutos;
+        this.horarioFuncionamento = horarioFuncionamento;
+    }
+
+    public void venderProduto(String produto) {
+        System.out.println("Vendendo produto: " + produto);
+    }
+
+    public void adicionarProduto(String produto) {
+        System.out.println("Adicionando produto: " + produto);
+    }
+
+    public void listarProdutos() {
+        System.out.println("Produtos disponíveis: " + listaProdutos);
+    }
+}
